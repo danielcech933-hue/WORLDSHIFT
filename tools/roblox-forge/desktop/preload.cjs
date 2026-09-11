4144ffe4c4e4f45f522087c4e9bfaf2d3a40d873
danielcech933-hue <danielcech933@gmail.com>
2026-09-11 20:14:05 +0200

    Fix ROBLOX FORGE packaged preload compatibility

---
+++ tools/roblox-forge/desktop/preload.cjs
@@ -0,0 +1,20 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('forge', {
+  info: () => ipcRenderer.invoke('app-info'),
+  status: () => ipcRenderer.invoke('process-status'),
+  project: () => ipcRenderer.invoke('project-get'),
+  chooseProject: () => ipcRenderer.invoke('project-choose'),
+  resetProject: () => ipcRenderer.invoke('project-reset'),
+  studioInstall: () => ipcRenderer.invoke('studio-install'),
+  studioHealth: () => ipcRenderer.invoke('studio-health'),
+  start: id => ipcRenderer.invoke('process-start', id),
+  stop: id => ipcRenderer.invoke('process-stop', id),
+  startStack: () => ipcRenderer.invoke('stack-start'),
+  stopStack: () => ipcRenderer.invoke('stack-stop'),
+  input: (id, value) => ipcRenderer.invoke('process-input', id, value),
+  openFolder: () => ipcRenderer.invoke('open-folder'),
+  onLog: callback => ipcRenderer.on('process-log', (_event, payload) => callback(payload)),
+  onExit: callback => ipcRenderer.on('process-exit', (_event, payload) => callback(payload)),
+  onError: callback => ipcRenderer.on('app-error', (_event, payload) => callback(payload)),
+});
