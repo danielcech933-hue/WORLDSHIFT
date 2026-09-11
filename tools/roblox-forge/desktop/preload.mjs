d1238b63b3cc1f1d5f05066693b6f28afb04c781
danielcech933-hue <danielcech933@gmail.com>
2026-09-11 19:33:04 +0200

    forge: expose project controls

--- tools/roblox-forge/desktop/preload.mjs
+++ tools/roblox-forge/desktop/preload.mjs
@@ -1,13 +1,13 @@
 import { contextBridge, ipcRenderer } from 'electron';
-
 contextBridge.exposeInMainWorld('forge', {
   status: () => ipcRenderer.invoke('process-status'),
+  project: () => ipcRenderer.invoke('project-get'),
+  chooseProject: () => ipcRenderer.invoke('project-choose'),
   start: id => ipcRenderer.invoke('process-start', id),
   stop: id => ipcRenderer.invoke('process-stop', id),
   startStack: () => ipcRenderer.invoke('stack-start'),
   stopStack: () => ipcRenderer.invoke('stack-stop'),
   input: (id, value) => ipcRenderer.invoke('process-input', id, value),
-  openFolder: () => ipcRenderer.invoke('open-folder'),
   onLog: callback => ipcRenderer.on('process-log', (_event, payload) => callback(payload)),
   onExit: callback => ipcRenderer.on('process-exit', (_event, payload) => callback(payload))
 });
