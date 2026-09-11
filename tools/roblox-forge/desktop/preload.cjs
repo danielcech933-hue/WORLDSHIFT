b7853e587bcc0f40310dff16bc73ebf922f29b5c
danielcech933-hue <danielcech933@gmail.com>
2026-09-11 21:31:43 +0200

    feat: expose complete Forge workspace API

--- tools/roblox-forge/desktop/preload.cjs
+++ tools/roblox-forge/desktop/preload.cjs
@@ -1,20 +1,2 @@
 const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('forge', {
-  info: () => ipcRenderer.invoke('app-info'),
-  status: () => ipcRenderer.invoke('process-status'),
-  project: () => ipcRenderer.invoke('project-get'),
-  chooseProject: () => ipcRenderer.invoke('project-choose'),
-  resetProject: () => ipcRenderer.invoke('project-reset'),
-  studioInstall: () => ipcRenderer.invoke('studio-install'),
-  studioHealth: () => ipcRenderer.invoke('studio-health'),
-  start: id => ipcRenderer.invoke('process-start', id),
-  stop: id => ipcRenderer.invoke('process-stop', id),
-  startStack: () => ipcRenderer.invoke('stack-start'),
-  stopStack: () => ipcRenderer.invoke('stack-stop'),
-  input: (id, value) => ipcRenderer.invoke('process-input', id, value),
-  openFolder: () => ipcRenderer.invoke('open-folder'),
-  onLog: callback => ipcRenderer.on('process-log', (_event, payload) => callback(payload)),
-  onExit: callback => ipcRenderer.on('process-exit', (_event, payload) => callback(payload)),
-  onError: callback => ipcRenderer.on('app-error', (_event, payload) => callback(payload)),
-});
+contextBridge.exposeInMainWorld('forge',{info:()=>ipcRenderer.invoke('app-info'),status:()=>ipcRenderer.invoke('process-status'),project:()=>ipcRenderer.invoke('project-get'),chooseProject:()=>ipcRenderer.invoke('project-choose'),resetProject:()=>ipcRenderer.invoke('project-reset'),studioInstall:()=>ipcRenderer.invoke('studio-install'),studioHealth:()=>ipcRenderer.invoke('studio-health'),studioState:()=>ipcRenderer.invoke('studio-state'),diagnostics:()=>ipcRenderer.invoke('diagnostics'),workspaceGet:()=>ipcRenderer.invoke('workspace-get'),workspaceSave:d=>ipcRenderer.invoke('workspace-save',d),agentsList:()=>ipcRenderer.invoke('agents-list'),agentRun:p=>ipcRenderer.invoke('agent-run',p),agentStop:id=>ipcRenderer.invoke('agent-stop',id),start:id=>ipcRenderer.invoke('process-start',id),stop:id=>ipcRenderer.invoke('process-stop',id),startStack:()=>ipcRenderer.invoke('stack-start'),stopStack:()=>ipcRenderer.invoke('stack-stop'),input:(id,v)=>ipcRenderer.invoke('process-input',id,v),openFolder:()=>ipcRenderer.invoke('open-folder'),onLog:cb=>ipcRenderer.on('process-log',(_e,p)=>cb(p)),onExit:cb=>ipcRenderer.on('process-exit',(_e,p)=>cb(p)),onError:cb=>ipcRenderer.on('app-error',(_e,p)=>cb(p))});
